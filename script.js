556575542aa000b1f4bc245b3dd6e3ca198069ce
Franklin Ezemmouka <kedgraphix@gmail.com>
2023-04-06 14:26:57 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+
+    count += 1
+    countEl.textContent = count
+    
+}
+
+function save() {
+
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    
+   
+}
+
+function reset() {
+    count = 0
+    countEl.innerHTML = 0
+    saveEl.innerHTML = "previous entries:"
+
+}
+
